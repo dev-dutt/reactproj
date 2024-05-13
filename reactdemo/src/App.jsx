9ec791b5ef3648bb60253d31fe456c44b89c4e3f
unknown <er.devdp@gmail.com>
2024-05-13 18:33:15 +0530

    new

--- reactdemo/src/App.jsx
+++ reactdemo/src/App.jsx
@@ -1,9 +1,36 @@
-
+import { useEffect,useState } from "react";
 
 function App() {
 
+  let [counter,setcounter]=useState(19);
+  // let counter = 5;
+
+  function addvalue(){
+    // counter = counter+1;
+    if(counter<20){
+      setcounter(counter+1);
+      console.log('add',counter);
+    }
+    
+    
+  }
+
+  const subvalue=()=>{
+    if(counter > 0){
+      setcounter(counter-1);
+      console.log('sub',counter);
+    }
+   
+    
+
+  }
+
   return (
-   <h1>Hello Dev</h1>
+    <>
+   <h1>Value : {counter}</h1>
+   <button onClick={addvalue}>ADD</button>
+   <button onClick={subvalue}>SUB</button>
+   </>
   )
 }
 
--- reactdemo/src/main.jsx
+++ reactdemo/src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// const user = 'Rahul';
+
+// const reactel = React.createElement(
+//   'p',
+//   {},
+//   'this is good',
+//   user
+// )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
